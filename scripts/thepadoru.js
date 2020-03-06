59bc7069525b2c88c4c8fb7a094671725404b634
AstolfoSempai <59913721+AstolfoSempai@users.noreply.github.com>
2020-03-06 19:03:11 -0300

    Update thepadoru.js

--- scripts/thepadoru.js
+++ scripts/thepadoru.js
@@ -1,6 +1,6 @@
 const basep = extendContent(TurretType, "thepadoru",
 {
 draw: function(tile){
-Draw.rect(Core.atlas.find("JustPADORU-basemalapadoru"), tile.drawx(), tile.drawy());
+Draw.rect(Core.atlas.find("padoru-basemalapadoru"), tile.drawx(), tile.drawy());
 }.
 });
