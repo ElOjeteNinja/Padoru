58f921bd94ea6e1e7ec99f1c4b17b2023afa843f
AstolfoSempai <59913721+AstolfoSempai@users.noreply.github.com>
2020-03-06 18:05:11 -0300

    Update scatter-silo.js

--- scripts/scatter-silo.js
+++ scripts/scatter-silo.js
@@ -3,7 +3,7 @@
 //create a simple shockwave effect
 const siloLaunchEffect = newEffect(20, e => {
     Draw.color(Color.white, Color.lightGray, e.fin()); //color goes from white to light gray
-    Lines.stroke(e.fout() * 10); //line thickness goes from 3 to 0
+    Lines.stroke(e.fout() * 150); //line thickness goes from 3 to 0
     Lines.circle(e.x, e.y, e.fin() * 100); //draw a circle whose radius goes from 0 to 100
 });
 
@@ -25,7 +25,7 @@ const silo = extendContent(Block, "scatter-silo", {
             Effects.effect(siloLaunchEffect, tile)
 
             //create 10 bullets at this tile's location with random rotation and velocity/lifetime
-            for(var i = 0; i < 100; i++){
+            for(var i = 0; i < 2; i++){
                 Calls.createBullet(Bullets.flakExplosive, tile.getTeam(), tile.drawx(), tile.drawy(), Mathf.random(360), Mathf.random(0.5, 1.0), Mathf.random(0.2, 1.0))
             }
             //triggering consumption makes it use up the items it requires
